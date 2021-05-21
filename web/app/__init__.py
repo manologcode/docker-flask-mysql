da3765d4db84e7926b6e1bc580c1445266dbb50e
Manuel Gomez Mateo <creawww@gmail.com>
2021-05-21 13:51:45 +0200

    changes in nginx container

--- web/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-def create_app(settings_module='config.dev'):
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
--- web/entrypoint.py
+++ web/entrypoint.py
@@ -1,10 +1,8 @@
-import os
+from flask import Flask
 
-from flask import send_from_directory
+app = Flask(__name__)
 
-from app import create_app
-
-
-settings_module = os.getenv('APP_SETTINGS_MODULE')
-app = create_app(settings_module)
+@app.route('/')
+def index():
+    return "This is an example app"
 
